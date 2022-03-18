8d66b785b1b814db7d9ee4a3a2cc1c102ee83935
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 14:18:38 -0400

    frontend

---
+++ terraform-mutable/main.tf
@@ -0,0 +1,8 @@
+module "app-module" {
+  source = "git::https://github.com/vasanthi-dev/terraform-mutable-app-module.git"
+  INSTANCE_COUNT = var.INSTANCE_COUNT
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  COMPONENT = var.COMPONENT
+  ENV = var.ENV
+  APP_VERSION = var.APP_VERSION
+}
